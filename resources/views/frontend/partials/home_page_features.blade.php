26d7d9538ac7ece174b71e5db9b82f8b6c46badf
sanjayaharshana <sanjayaharasana@gmail.com>
2021-09-09 22:32:38 +0530

    Admin Function

---
+++ resources/views/frontend/partials/home_page_features.blade.php
@@ -0,0 +1,130 @@
+
+
+
+<div class="" style="margin-top: 50px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                    <h5> {{ translate('Art Craft') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 280px;">
+                            <img src="{{ url('imgs/Art _ Craft.jpg') }}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Art Craft') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5>{{ translate('Bathik') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 280px;">
+                            <img src="{{ url('imgs/Bathik.jpg') }}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Bathik') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5> {{ translate('Cane Furniture') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 280px;">
+                            <img src="{{ url('imgs/Cane Furniture .jpg') }}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Cane Furniture') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5>{{ translate('Garden Center') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 280px;">
+                            <img src="{{ url('imgs/Garden Center.jpg') }}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Garden Center') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5>{{ translate('Gift Cards') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 280px;">
+                            <img src="{{ url('imgs/gift card.jpg') }}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Gift Cards') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body" style="text-align: center">
+                        <h5>{{ translate('Health and Wellness') }}</h5><br>
+                        <figure class="c4-izmir c4-border-corners-1 c4-image-zoom-out c4-gradient-bottom" tabindex="0" style="--primary-color: #000000;--secondary-color: #000000;--text-color: #f0f0f0;--border-color: #ffffff;--image-opacity: 60%;width: 280px;">
+                            <img src="{{ url('imgs/health and wellness.jpg') }}" alt="Sample Image">
+                            <figcaption class="c4-layout-top-left">
+                                <div class="c4-reveal-down">
+                                    <div>
+                                        <h2 style="margin-bottom: 5px;">
+                                            {{ translate('Health and Wellness') }}
+                                        </h2>
+                                        <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure><br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
